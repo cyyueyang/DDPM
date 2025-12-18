408357ab86c92122c90bf7f15d27ad709b697274
yueyangcheng <yueyangc67@gmail.com>
2025-12-18 14:17:02 +0800

    finish schedule

--- ddpm/diffusion.py
+++ ddpm/diffusion.py
@@ -122,6 +122,23 @@ class GaussianDiffusion(nn.Module):
 
         return self.get_loss(x, t)
 
+def generate_cosine_schedule(T, s=0.008):
+    def f(t, T):
+        return np.cos((t / T + s) / (1 + s) * (np.pi / 2.0)) ** 2
+
+    alphas = []
+    f_0 = f(0, T)
+    for t in range(T + 1):
+        alphas.append(f(t, T) / f_0)
+
+    betas = []
+    for t in range(1, T + 1):
+        betas.append(min(1 - alphas[t] / alphas[t-1], 0.999))
+
+    return np.array(betas)
+
+def generate_linear_schedule(T, low, high):
+    return torch.linspace(low, high, T, device=T.device)
 
 
 
